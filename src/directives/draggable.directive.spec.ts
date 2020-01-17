3e12530754adb786aaffe3353de3df1503642247
sshijun <sshijun@vmware.com>
2020-01-17 15:50:24 +0800

    Add github action suport
    Signed-off-by: sshijun <sshijun@vmware.com>

--- src/directives/draggable.directive.spec.ts
+++ src/directives/draggable.directive.spec.ts
@@ -1,8 +1,9 @@
 import { DraggableDirective } from './draggable.directive';
+import { ElementRef } from '@angular/core';
 
 describe('DraggableDirective', () => {
   it('should create an instance', () => {
-    const directive = new DraggableDirective();
+    const directive = new DraggableDirective(null);
     expect(directive).toBeTruthy();
   });
 });
